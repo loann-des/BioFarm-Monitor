9ccc2395c370af3506780094ecfb20f970c832f8
pcharon01 <152666303+pcharon01@users.noreply.github.com>
2026-01-11 18:38:14 +0100

    Added test stub for web_app.fonction module

---
+++ test/test_fonction.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+import os
+import sys
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), "../"))
+
+import unittest
+
+class DataFunctionsUnitTests:
+    # TODO: test first
+    def test_first(self):
+        pass
+
+    # TODO: test last
+    def test_last(self):
+        pass
+
+    # TODO: test mystrftime
+    def test_my_strftime(self):
+        pass
+
+    # TODO: test parse_date
+    def test_parse_date(self):
+        pass
+
+    # TODO: test date_to_str
+    def test_date_to_str(self):
+        pass
+
+    # TODO: test sum_date_to_str
+    def test_sum_date_to_str(self):
+        pass
+
+    # TODO: test substract_date_to_str
+    def test_substract_date_to_str(self):
+        pass
+
+    # TODO: test format_bool_fr
+    def test_format_bool_fr(self):
+        pass
+
+    # TODO: test parse_bool
+    def test_parse_bool(self):
+        pass
+
+    # TODO: test day_delta
+    def test_day_delta(self):
+        pass
+
+class CaresUtilityFunctionsUnitTests:
+    # TODO: test nb_cares_year
+    def test_nb_cares_years(self):
+        pass
+
+    # TODO: test nb_cares_years_of_cow
+    def test_nb_cares_years_of_cow(self):
+        pass
+
+    # TODO: test remaining_cares_on_year
+    def test_remaining_care_on_year(self):
+        pass
+
+    # TODO: test new_available_care
+    def test_new_available_care(self):
+        pass
+
+class PharmaUtilityFunctionsUnitTests:
+    # TODO: test get_pharma_list
+    def test_get_pharma_list(self):
+        pass
+
+    # TODO: test get_pharma_len
+    def test_get_pharma_len(self):
+        pass
+
+    # TODO: test sum_pharmacie_len
+    def test_sum_pharmacie_in(self):
+        pass
+
+    # TODO: test sum_pharmacie_used
+    def test_sum_pharmacie_used(self):
+        pass
+
+    # TODO: test sum_calf_used
+    def test_sum_calf_used(self):
+        pass
+
+    # TODO: test sum_dlc_left
+    def test_sum_dlc_left(self):
+        pass
+
+    # TODO: test sum_pharmacie_left
+    def test_sum_pharmacie_left(self):
+        pass
+
+    # TODO: test remaining_pharmacie_stock
+    def test_remaining_pharmacie_stock(self):
+        pass
+
+    # TODO: test get_history_pharmacie
+    def test_get_history_pharmacie(self):
+        pass
+
+    # TODO: test update_pharmacie_years
+    def test_update_pharmacie_year(self):
+        pass
+
+    # TODO: test pharmacie_to_csv
+    def test_pharmacie_to_csv(self):
+        pass
+
+    # TODO: test remaining_care_to_excel
+    def test_remaining_care_to_excel(self):
+        pass
+
+class HerdUtilityFunctionsUnitTests:
+    # TODO: test get_all_dry_date
+    def test_get_all_dry_date(self):
+        pass
+
+    # TODO: test get_all_calving_preparation_date
+    def test_get_all_calving_preparation_date(self):
+        pass
+
+    # TODO: test get_all_calving_date
+    def test_get_all_calving_date(self):
+        pass
+
+if (__name__ == "__main__"):
+    unittest.main()
