fc8ee8360b65d3e862fb99ce7e4a95813d75200d
pcharon01 <152666303+pcharon01@users.noreply.github.com>
2026-02-18 01:56:31 +0100

    Added cow acquisition interface

--- web_app/static/js/herd.js
+++ web_app/static/js/herd.js
@@ -1,4 +1,54 @@
 window.addEventListener("load", async () => {
+  const addCowButton = document.querySelector("button#add-cow-button");
+  const overlay = document.querySelector("div#overlay");
+  const popup = document.querySelector("div.popup");
+  const popupCloseButton = document.querySelector("div.popup div.popup-header button");
+
+  addCowButton.addEventListener("click", () => {
+    overlay.style.display = "block";
+    popup.style.display = "flex";
+  });
+
+  popupCloseButton.addEventListener("click", () => {
+    overlay.style.display = "none";
+    popup.style.display = "none";
+  });
+
+  const popupForm = document.querySelector("div.popup form");
+
+  popupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const action = e.target.getAttribute("action");
+    const method = e.target.getAttribute("method");
+    const data = new FormData(e.target);
+
+    try {
+      const response = await fetch(action, {
+        method: method,
+        body: data
+      });
+
+      const contentType = response.headers.get("Content-Type");
+
+      if (!contentType.includes("application/json")) {
+        console.error(`Unexpected response: expected application/json, got ${contentType}`);
+        // TODO: Proper error display on the user's side
+        return;
+      }
+
+      console.log(response);
+
+      updateHerd();
+    } catch (error) {
+      console.error(`AJAX request failed due to ${error}`);
+      // TODO: Proper error display on the user's side
+    }
+
+    overlay.style.display = "none";
+    popup.style.display = "none";
+  });
+
   updateHerd();
 });
 
